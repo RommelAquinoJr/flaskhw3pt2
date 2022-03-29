d0e7f6fa50d614a904690d84e843e448ee2cd050
RommelAquinoJr <100814020+RommelAquinoJr@users.noreply.github.com>
2022-03-28 22:12:31 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,9 @@
 from app import myobj
 from flask import render_template, flash
 
-
 @myobj.route("/")
 
 def home():
-	#name = {'name': 'Lisa'}
-	#city_names = ['Paris', 'London', 'Rome', 'Tahiti']
 	return render_template('home.html',city_names = city_names, name=name)
-name = ' Lisa'
+name = 'Lisa'
 city_names = ['Paris', 'London', 'Rome', 'Tahiti']
